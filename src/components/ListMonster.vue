e369925dec18207c532303a9a482242d3eb822cc
NicholasFerrari <nicholasferrari95@gmail.com>
2024-08-28 17:52:34 +0200

    List
    List

--- src/components/ListMonster.vue
+++ src/components/ListMonster.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="list-wrapper">
-    <ul v-for="item in 5" :key="item">
+    <ul v-for="item in monsters" :key="item">
       <li class="card-mmonster">
-        <p>ciao</p>
+        <p>{{ item }}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'ListMonster',
+
+  data() {
+    return {
+      monsters: [],
+    }
+  },
+  mounted() {
+    this.monsters = this.getMonsterList;
+  },
+  methods: {
+    getMonsterList() {
+      axios.get('https://api.example.com/monsters')
+        .then(response => {
+          this.monsters = response.data;
+        })
+        .catch(error => {
+          console.error('Errore durante il fetch dei mostri:', error);
+        });
+  },
+  computed: {
+
+  },
+}
 }
 </script>
